fa51d1dc1393af32c54bd887d8a6f0af8536314d
Familia Reyes Villa <familiareyesvilla@MacBook-Air-de-Familia.local>
2023-07-14 08:49:34 -0600

    primer commit sistema examenes frontend

---
+++ src/app/pages/signup/signup.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import {FormGroup,FormControl, Validators} from '@angular/forms';
+import { UserService } from '../../services/user.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import Swal from 'sweetalert2';
+
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent {
+  hide = true;
+public user = {
+  username : '',
+  password : '',
+  nombre : '',
+  apellido : '',
+  email : '',
+  telefono : ''
+}
+constructor (private userService:UserService,private snack:MatSnackBar){}
+
+formSubmit(){
+  console.log(this.user);
+  if(this.user.username == ''|| this.user.username == null){
+    this.snack.open('El nombre de usuario es requerido','Aceptar',{
+      duration:3000,
+      verticalPosition:'top',
+      horizontalPosition:'right'
+    });
+    return;
+  }
+
+  this.userService.añadirUsuario(this.user).subscribe(
+    (data)=>{
+      console.log(data);
+      Swal.fire('Usuario guardado','Usuario registrado con exito','success');
+    },(error)=>{
+      console.log(error);
+      this.snack.open('Ha ocurrido un error en el sistema.','Aceptar',{
+        duration:3000,
+        verticalPosition:'top',
+        horizontalPosition:'center'
+      });
+    }
+  )
+
+}
+
+}
